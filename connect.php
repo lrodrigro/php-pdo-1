9e32ceef6276d5cca60b1239a07d12302e859aa2
Luis Rodriguez <luis.rr.3@gmail.com>
2020-06-03 09:48:36 +0200

    Modifications qui seront validées : 	nouveau fichier : connect.php 	nouveau fichier : index.php

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+//define("HOST", 'localhost');
+//define("DB", 'test');
+//define("USER", 'root');
+//define("PASS", 'pass';
+
+define('DSN', 'mysql:host=localhost;dbname=pdo_quest');
+define('USER', 'test');
+define('PASS', '@Test1234');
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once ('connect.php');
+
+function listFriends() : variant_mod
+{
+
+}
+
+$pdo = new \PDO(DSN, USER, PASS);
+
+$query = "SELECT * FROM friend";
+$statement = $pdo->query($query);
+$friends = $statement->fetchAll();
+
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Mes amis</title>
+    <style>
+        .error{
+            padding:20px;
+            border-radius:4px;
+            border:1px solid red;
+            background:tomato;
+        }
+    </style>
+</head>
+<body>    
+    <br>
+    <?php
+    echo "<ul>" . PHP_EOL;
+    echo "<li>";
+    echo "Prénom" . ' | ' . "Nom";
+    echo "</li>" . PHP_EOL;
+    foreach($friends as $friend)
+    {
+        echo "<li>" . $friend['firstname'] . ' | ' . $friend['lastname'] . "</li>" . PHP_EOL;
+    }        
+    echo "</ul>"; 
+    ?>
+    <br>
+    <form  action="index.php"  method="post">
+        <div>
+            <label for="firstname">Prénom :</label>
+            <input type="text"  id="firstname" name="firstname" required>
+        </div>
+        <div>
+            <label for="lastname">Nom :</label>
+            <input type="text" id="lastname" name="lastname" required>
+        </div>
+        <div >
+            <button type="submit" id="submit" name="add">Submit</button>
+        </div>
+    </form>
+    <?php 
+     
+    if( isset( $_POST['firstname'] ) ) {     
+        $firstname = trim($_POST['firstname']);
+        $lastname = trim($_POST['lastname']);
+    
+        $query = 'INSERT INTO friend (firstname, lastname) VALUES (:firstname, :lastname)';
+        $stm = $pdo->prepare($query);
+    
+        $stm->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
+        $stm->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
+    
+        $stm->execute();
+
+        $_POST['firstname'] = '';
+        $_POST['lastname'] = '';
+
+        //redirect
+        header('location: index.php');
+    }
+    ?>
+
+</body>
+</html>
